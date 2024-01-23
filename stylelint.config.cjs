e852c7beb0e282aff706c5cddd987f716bb4165d
nomiad <nomiadcode@gmail.com>
2024-01-23 17:46:30 +0100

    added initial style commit

---
+++ stylelint.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-standard-scss',
+    'stylelint-config-recommended-vue/scss',
+  ],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {
+    'no-duplicate-selectors': null,
+    'selector-class-pattern': null,
+    'scss/no-global-function-names': null,
+    'scss/at-import-no-partial-leading-underscore': null,
+    'block-no-empty': null,
+    'property-no-unknown': null,
+    'scss/comment-no-empty': null
+  },
+}
